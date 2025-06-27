de85212926a34e930005df4232a8f6bcb912c9d2
Yoon-Mac <sryoowk@gmail.com>
2025-06-27 15:02:18 +0900

    Rename: #13 Category 아이폰 아이패드 변수 lowercase화

--- UCDAppleStore/UCDAppleStore/Models/Category.swift
+++ UCDAppleStore/UCDAppleStore/Models/Category.swift
@@ -2,8 +2,8 @@
 import Foundation
 
 enum Category: String, Codable {
-    case iPhone
-    case iPad
+    case iphone
+    case ipad
     case macBook = "MacBook"
     case mac = "Mac"
     case accessories = "Accessories"
