1ca3a93418e84f17eb1ebe1bd239515a6f48b136
jiyoungsophia <sophiathedev@gmail.com>
2025-06-27 18:01:05 +0900

    Chore: #15-  카테고리 열거형 확장
    - 카테고리 뷰와 뷰모델에서 사용하기 위해 확장하였습니다

--- UCDAppleStore/UCDAppleStore/Models/Category.swift
+++ UCDAppleStore/UCDAppleStore/Models/Category.swift
@@ -1,10 +1,29 @@
 
 import Foundation
 
-enum Category: String, Codable {
+enum Category: String, Codable, CaseIterable {
     case iphone
     case ipad
     case macBook = "MacBook"
     case mac = "Mac"
     case accessories = "Accessories"
+    
+    var titleName: String {
+        switch self {
+        case .iphone:
+            return "iPhone"
+        case .ipad:
+            return "iPad"
+        case .macBook:
+            return "MacBook"
+        case .mac:
+            return "Mac"
+        case .accessories:
+            return "악세서리"
+        }
+    }
+    
+    static var allCategories: [Category] {
+        return Category.allCases
+    }
 }
