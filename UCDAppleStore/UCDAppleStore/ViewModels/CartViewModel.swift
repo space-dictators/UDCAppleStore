bbece5fb0dc06e1d148bd326dd32f206cbe024c8
Yoon-Mac <sryoowk@gmail.com>
2025-06-27 19:33:26 +0900

    Feat: #17 수량 증가, 감소, 취소하기(장바구니 비우기) 버튼 대응 함수 구현

--- UCDAppleStore/UCDAppleStore/ViewModels/CartViewModel.swift
+++ UCDAppleStore/UCDAppleStore/ViewModels/CartViewModel.swift
@@ -2,11 +2,16 @@
 import Foundation
 
 final class CartViewModel {
-    private(set) var testCart: Cart
+    
+    // MARK: Properties
+    
+    private(set) var cartItems: [CartItem]
+
+    // MARK: Initailizers
 
     init() {
-        // 하드코딩된 테스트 데이터
-        let testCartItems: [CartItem] = [
+        // 테스트용 하드코딩 데이터
+        cartItems = [
             CartItem(
                 product: Product(
                     id: 1,
@@ -35,18 +40,61 @@ final class CartViewModel {
                     price: 529_000,
                     imageURL: URL(string: "https://store.storeimages.cdn-apple.com/...")!
                 ),
+                quantity: 2
+            ),
+            CartItem(
+                product: Product(
+                    id: 12,
+                    name: "MacBook Air M4 13〃 16GB 256GB",
+                    category: .ipad,
+                    price: 1_590_000,
+                    imageURL: URL(string: "https://store.storeimages.cdn-apple.com/...")!
+                ),
                 quantity: 1
             ),
         ]
-
-        testCart = Cart(items: testCartItems)
     }
 
+    // MARK: Properties
+
     var totalPriceText: String {
-        "\(testCart.totalPrice)원"
+        "\(cartItems.reduce(0) { $0 + $1.totalPrice })원"
     }
 
     var purchaseButtonTitle: String {
-        "총 \(testCart.totalCount)개 결제하기"
+        "총 \(cartItems.reduce(0) { $0 + $1.quantity })개 결제하기"
+    }
+
+    // MARK: Methods
+    
+    // 수량 증가
+    func changeQuantity(for product: Product, isIncrease: Bool) {
+        // product id를 이용해 어떤 상품의 인덱스인지 찾아서 할당
+        guard let index = cartItems.firstIndex(where: { $0.product.id == product.id }) else {
+            return // 비어있을 경우 아무것도 하지 않음
+        }
+
+        var item = cartItems[index]
+
+        if isIncrease { // true, +버튼을 눌렀을 때
+            if item.quantity >= 10 { // 이미 10개 이상이라면
+                // TODO: Alert - 최대 수량(10개) 초과
+                return
+            }
+            item.quantity += 1
+        } else { // false, - 버튼을 눌렀을 때
+            if item.quantity == 0 { // 이미 0개일 경우
+                // TODO: Alert - 최소 수량(0개) 미만
+                return
+            }
+            item.quantity -= 1
+        }
+
+        cartItems[index] = item // 바뀐 값 재할당
+    }
+    
+    // 카트 비우기
+    func clearCart() {
+        cartItems.removeAll()
     }
 }
