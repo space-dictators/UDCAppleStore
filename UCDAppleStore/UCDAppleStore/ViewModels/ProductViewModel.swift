4b3580cdebd7f9c778b65ed206de103d9c22fd7a
jiyoungsophia <sophiathedev@gmail.com>
2025-06-30 13:35:13 +0900

    Refactor: #29- ProductViewModel 수정
    - 뷰에 있던 필터링해주는 로직 ViewModel로 이동

--- UCDAppleStore/UCDAppleStore/ViewModels/ProductViewModel.swift
+++ UCDAppleStore/UCDAppleStore/ViewModels/ProductViewModel.swift
@@ -8,19 +8,27 @@
 import Foundation
 
 class ProductViewModel {
+    private var allProducts: [Product] = []
     private(set) var products: [Product] = []
-
+    
     var onDataUpdated: (() -> Void)?
-
+    
     func fetchProducts() {
-        products = DataService.shared.loadProducts()
+        let loadedProducts = DataService.shared.loadProducts()
+        allProducts = loadedProducts
+        products = loadedProducts
         onDataUpdated?()
     }
-
+    
+    func filterProducts(by category: Category) {
+        products = allProducts.filter { $0.category == category }
+        onDataUpdated?()
+    }
+    
     func numberOfItems() -> Int {
         return products.count
     }
-
+    
     func product(at index: Int) -> Product? {
         guard index >= 0, index < products.count else { return nil }
         return products[index]
